132d6c339fe6e4891c34092c07353a12df176703
Dawn Schumacher <dawn@berkeley.edu>
2025-10-05 17:28:55 -0700

    gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("com.google.api-client:google-api-client:2.0.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
+    implementation("com.google.code.gson:gson:2.13.2")
 }
 
 tasks.withType(Jar::class) {
