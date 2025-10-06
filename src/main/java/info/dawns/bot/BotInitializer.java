f5c924bfc79770da4c2d083957c055903bd77bda
Dawn Schumacher <dawn@berkeley.edu>
2025-10-05 17:25:23 -0700

    Classpath business

--- src/main/java/info/dawns/bot/BotInitializer.java
+++ src/main/java/info/dawns/bot/BotInitializer.java
@@ -6,26 +6,18 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.Reader;
+import java.io.*;
 import java.util.Collections;
 
 public class BotInitializer {
     public static JDA initializeBot() {
-        try {
-            ClassLoader loader = BotInitializer.class.getClassLoader();
-            Reader reader = new FileReader(loader.getResource(".env/discord/credentials.json").getFile());
-            String discordToken = (new Gson()).fromJson(reader, DiscordCredentials.class).getToken();
-            return JDABuilder.createLight(discordToken, Collections.emptyList())
-                    .enableIntents(
-                            GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
-                    .addEventListeners(new BotCommandListener())
-                    .build();
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        ClassLoader loader = BotInitializer.class.getClassLoader();
+        Reader reader = new InputStreamReader(loader.getResourceAsStream(".env/discord/credentials.json"));
+        String discordToken = (new Gson()).fromJson(reader, DiscordCredentials.class).getToken();
+        return JDABuilder.createLight(discordToken, Collections.emptyList())
+                .enableIntents(
+                        GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
+                .addEventListeners(new BotCommandListener())
+                .build();
     }
 }
